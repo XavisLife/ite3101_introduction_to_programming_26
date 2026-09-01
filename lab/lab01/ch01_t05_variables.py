58e876fbdda8bfc64fa1ea0ed051c11d8cdff00e
XavisGameDev <xavislife2027@outlook.com>
2026-09-01 03:26:11 +0000

    [skip actions] lab/lab01/ch01_t01_hello_world.py

--- lab/lab01/ch01_t05_variables.py
+++ lab/lab01/ch01_t05_variables.py
@@ -1,1 +1,3 @@
-todays_date = "1"
+# It is hard-coded but not sure if we could import the datetime library for this task..
+# We could use datetime.datetime.now():%d/%m/%Y to present the up-to-date current date.
+todays_date = "01/09/2026" 
