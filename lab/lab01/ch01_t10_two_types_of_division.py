700da6801571acf9bf0e934ff9d3c25df09f062a
XavisGameDev <xavislife2027@outlook.com>
2026-09-01 14:47:41 +0000

    [skip actions] lab/lab01/ch01_t10_two_types_of_division.py

--- lab/lab01/ch01_t10_two_types_of_division.py
+++ lab/lab01/ch01_t10_two_types_of_division.py
@@ -1,6 +1,6 @@
 cucumbers = 100
 num_people = 6
-whole_cucumbers_per_person = (int)(cucumbers / num_people
+whole_cucumbers_per_person = (int)(cucumbers / num_people)
 print(whole_cucumbers_per_person)
 float_cucumbers_per_person = (float)(whole_cucumbers_per_person)
 print(float_cucumbers_per_person)
