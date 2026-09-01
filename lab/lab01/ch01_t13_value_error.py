134e665809a8b08950dfe99af9db7dac889c4c04
XavisGameDev <xavislife2027@outlook.com>
2026-09-01 15:21:19 +0000

    [skip actions] lab/lab01/ch01_t13_value_error.py

--- lab/lab01/ch01_t13_value_error.py
+++ lab/lab01/ch01_t13_value_error.py
@@ -1,4 +1,4 @@
 float_1 = 0.25
 float_2 = 40.0
 product = (str)(float_1 * float_2) # Calculate float1 multiplies by float 2 and type cast the result to string
-big_string = "The product was " + ((float)(product)
+big_string = "The product was " + ((float)(product))
