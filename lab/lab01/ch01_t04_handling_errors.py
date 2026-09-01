0926cfe482318d67c10f19ccbdda5156de67ff16
XavisGameDev <xavislife2027@outlook.com>
2026-09-01 03:15:11 +0000

    [skip actions] lab/lab01/ch01_t04_handling_errors.py

--- lab/lab01/ch01_t04_handling_errors.py
+++ lab/lab01/ch01_t04_handling_errors.py
@@ -1,2 +1,2 @@
 print("How do you make a hot dog stand?") # This was a syntax error where the last quote is ' and first quote is ".
-# print(You take away its chair!)
+print(You take away its chair!)
