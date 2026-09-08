6435338a0a09283a382b1164460cba94b5969ea3
XavisGameDev <xavislife2027@outlook.com>
2026-09-08 02:17:27 +0000

    [skip actions] lab/lab02/ch02_t02_practice.py

--- lab/lab02/ch02_t02_practice.py
+++ lab/lab02/ch02_t02_practice.py
@@ -1,6 +1,6 @@
 # Assign your variables below, each on its own line!
 caesar = "Graham"
-
+praline = "John"
 
 # Put your variables above this line, and uncomment the following 3 line of codes.
 # print(caesar)
