59604ff826158be53e2385acd714c6f4f66a3504
XavisGameDev <xavislife2027@outlook.com>
2026-09-08 02:11:58 +0000

    lab/lab01/ch01_t04_handling_errors.py

--- lab/lab01/ch01_t04_handling_errors.py
+++ lab/lab01/ch01_t04_handling_errors.py
@@ -1,4 +1,4 @@
 # This was a syntax error where the last quote is ' and first quote is ".
 print("How do you make a hot dog stand?")
-# This statement didn't have open and close quotes for the string. It was a name error.
+# This statement didn't have open and close quotes for the string. It was a name error..
 print("You take away its chair!")
