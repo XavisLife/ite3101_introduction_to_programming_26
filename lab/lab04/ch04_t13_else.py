988c69dd76bf263a5dbe8a573582a9b0de4bbaa4
XavisGameDev <xavislife2027@outlook.com>
2026-09-08 06:15:58 +0000

    lab/lab04/ch04_t13_else.py

--- lab/lab04/ch04_t13_else.py
+++ lab/lab04/ch04_t13_else.py
@@ -5,7 +5,7 @@ def black_knight() -> bool:
     if answer == "'Tis but a scratch!":
         return True
     else:
-        return  # Make sure this returns False
+        return False # Make sure this returns False
 
 
 def french_soldier() -> bool:
