14ae84879a3ed372cd08ef498d0bd2bc699c266f
XavisGameDev <xavislife2027@outlook.com>
2026-09-08 02:18:21 +0000

    lab/lab02/ch02_t02_practice.py

--- lab/lab02/ch02_t02_practice.py
+++ lab/lab02/ch02_t02_practice.py
@@ -1,9 +1,9 @@
 # Assign your variables below, each on its own line!
 caesar = "Graham"
 praline = "John"
-viking = ""
+viking = "Teresa"
 
 # Put your variables above this line, and uncomment the following 3 line of codes.
-# print(caesar)
-# print(praline)
-# print(viking)
+print(caesar)
+print(praline)
+print(viking)
