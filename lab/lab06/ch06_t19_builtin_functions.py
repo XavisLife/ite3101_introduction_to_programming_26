322d162bffa2b856cf929a72591834ab769dbda7
XavisGameDev <xavislife2027@outlook.com>
2026-09-08 12:28:55 +0000

    [skip actions] lab/lab06/ch06_t19_builtin_functions.py

--- lab/lab06/ch06_t19_builtin_functions.py
+++ lab/lab06/ch06_t19_builtin_functions.py
@@ -1,3 +1,5 @@
 def distance_from_zero(number)
-    if type(number) == int or float:
-        return abs()
+   if type(number) == int or float:
+        return abs(number)
+    else:
+        return "Nope"
