66805ac7995bbfccf79971c9b4cb489a1270b24c
XavisGameDev <xavislife2027@outlook.com>
2026-09-09 04:08:29 +0000

    [skip actions] lab/lab08/ch08_t14_last_note.py

--- lab/lab08/ch08_t14_last_note.py
+++ lab/lab08/ch08_t14_last_note.py
@@ -3,6 +3,7 @@ inventory = {
     # Assigned a new list to 'pouch' key
     'pouch': ['flint', 'twine', 'gemstone'],
     'backpack': ['xylophone', 'dagger', 'bedroll', 'bread loaf']
+    
 }
 
 # Adding a key 'burlap bag' and assigning a list to it
