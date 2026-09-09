59f73de60d330ef15bed8badb545a55f89c721bf
XavisGameDev <xavislife2027@outlook.com>
2026-09-09 01:30:01 +0000

    [skip actions] lab/lab07/ch07_t06_never_know.py

--- lab/lab07/ch07_t06_never_know.py
+++ lab/lab07/ch07_t06_never_know.py
@@ -22,5 +22,5 @@ def rental_car_cost(days: int) -> int:
     return cost
 
 
-def trip_cost(city: str, days: int, spending_money) -> int:
+def trip_cost(city: str, days: int, spending_money: int) -> int:
     return rental_car_cost(days) + hotel_cost(days - 1) + plane_ride_cost(city)
