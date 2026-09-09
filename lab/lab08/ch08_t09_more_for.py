16408d51b4989ae7f8c62b5d82e919fa611aaa97
XavisGameDev <xavislife2027@outlook.com>
2026-09-09 03:52:34 +0000

    [skip actions] lab/lab08/ch08_t09_more_for.py

--- lab/lab08/ch08_t09_more_for.py
+++ lab/lab08/ch08_t09_more_for.py
@@ -2,7 +2,7 @@ start_list = [5, 3, 1, 2, 4]
 square_list = []
 
 # Your code here!
-for i in start_list:
-    square_list.append(i)
+for x in start_list:
+    square_list.append(x ** 2)
 
 print(square_list)
