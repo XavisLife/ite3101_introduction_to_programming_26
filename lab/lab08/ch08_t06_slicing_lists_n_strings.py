deca793535e432f4a1249d5dc0411e13a19f04da
XavisGameDev <xavislife2027@outlook.com>
2026-09-09 01:48:16 +0000

    [skip actions] lab/lab08/ch08_t06_slicing_lists_n_strings.py

--- lab/lab08/ch08_t06_slicing_lists_n_strings.py
+++ lab/lab08/ch08_t06_slicing_lists_n_strings.py
@@ -4,7 +4,7 @@ animals = "catdogfrog"
 cat = animals[0:4]
 
 # The fourth through sixth characters
-dog = animals[]
+dog = animals[4:7]
 
 # From the seventh character to the end
-frog = None
+frog = animals[7:]
