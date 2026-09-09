2555f2bd61ffdef44a8002d5482dbcf2502cccc1
XavisGameDev <xavislife2027@outlook.com>
2026-09-09 07:28:55 +0000

    [skip actions] lab/lab09/ch09_t13_checkout.py

--- lab/lab09/ch09_t12_stocking_out.py
+++ lab/lab09/ch09_t12_stocking_out.py
@@ -23,5 +23,5 @@ def compute_bill(food: List[str]) -> float:
     for item in food:
         if (stock[item] > 0):
             total += prices[item]
-            stock[item] - -
+            stock[item] -= 1
     return total
