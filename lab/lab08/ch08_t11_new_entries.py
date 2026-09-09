5fa8c88866fbf9269830659e45730d8ae1e93827
XavisGameDev <xavislife2027@outlook.com>
2026-09-09 04:00:04 +0000

    [skip actions] lab/lab08/ch08_t11_new_entries.py

--- lab/lab08/ch08_t11_new_entries.py
+++ lab/lab08/ch08_t11_new_entries.py
@@ -4,7 +4,8 @@ print(menu['Chicken Alfredo'])
 
 # Your code here: Add some dish-price pairs to menu!
 menu['Burrito'] = 17.93
-
+menu['Burrito'] = 17.93
+menu['Burrito'] = 17.93
 
 print("There are " + str(len(menu)) + " items on the menu.")
 print(menu)
