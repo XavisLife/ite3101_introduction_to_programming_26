6c9ed1f4891f8124fd522d4142a5b63f1687b129
XavisGameDev <xavislife2027@outlook.com>
2026-09-10 11:11:17 +0000

    [skip actions] lab/lab14/ch014_t09_censor.py

--- lab/lab14/ch014_t09_censor.py
+++ lab/lab14/ch014_t09_censor.py
@@ -1,2 +1,9 @@
 def censor(text: str, word: str) -> str:
+    words = text.split(" ")
+    asterisks = "*" * len(word)
     
+    for i in range(len(words)):
+        if words[i] == word:
+            words[i] = asterisks
+            
+    return " ".join(words)
