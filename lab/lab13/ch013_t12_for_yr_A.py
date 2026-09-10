756b9e3b1d31add2e14fb4183bf508c2d08f1df6
XavisGameDev <xavislife2027@outlook.com>
2026-09-10 03:28:44 +0000

    [skip actions] lab/lab13/ch013_t12_for_yr_A.py

--- lab/lab13/ch013_t12_for_yr_A.py
+++ lab/lab13/ch013_t12_for_yr_A.py
@@ -2,7 +2,9 @@ phrase = "A bird in the hand..."
 
 # Add your for loop
 for char in phrase:
-    if char == "A" 
+    if char == "A" or char == "a":
+        print "X"
+    else
 
 # Don't delete this print statement!
 print()
