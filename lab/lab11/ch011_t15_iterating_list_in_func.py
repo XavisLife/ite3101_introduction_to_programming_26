08b518d9a903ca6c09a30f077baff7c21d35a97b
XavisGameDev <xavislife2027@outlook.com>
2026-09-10 05:20:10 +0000

    [skip actions] lab/lab11/ch011_t16_str_in_list_in_func.py

--- lab/lab11/ch011_t15_iterating_list_in_func.py
+++ lab/lab11/ch011_t15_iterating_list_in_func.py
@@ -1,7 +1,8 @@
 n = [3, 5, 7]
 
+
 def total(numbers):
     result = 0
     for i in range(0, len(numbers)):
-        total += numbers[i]
-    return total
+        result += numbers[i]
+    return result
--- lab/lab11/ch011_t16_str_in_list_in_func.py
+++ lab/lab11/ch011_t16_str_in_list_in_func.py
@@ -1,5 +1,5 @@
 n = ["Michael", "Lieberman"]
 # Add your function here
-
+def join_strings(words)
 
 # print(join_strings(n))
