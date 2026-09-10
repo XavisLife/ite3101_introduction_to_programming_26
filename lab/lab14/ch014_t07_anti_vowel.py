3e367272fdf07815d4e91b297a967c8f99ce80dd
XavisGameDev <xavislife2027@outlook.com>
2026-09-10 10:49:19 +0000

    [skip actions] lab/lab14/ch014_t07_anti_vowel.py

--- lab/lab14/ch014_t07_anti_vowel.py
+++ lab/lab14/ch014_t07_anti_vowel.py
@@ -2,5 +2,5 @@ def anti_vowel(text):
     vowels = "aeiou"
     for character in text:
         if character.Lower() in vowels:
-            text = 
+            text = text.replace(character, "")
     return ''.join
