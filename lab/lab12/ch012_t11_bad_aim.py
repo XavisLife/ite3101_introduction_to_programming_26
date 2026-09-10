7acaa7d700258cc4d452aad3bc8d07da1515c953
XavisGameDev <xavislife2027@outlook.com>
2026-09-10 09:51:47 +0000

    [skip actions] lab/lab12/ch012_t11_bad_aim.py

--- lab/lab12/ch012_t11_bad_aim.py
+++ lab/lab12/ch012_t11_bad_aim.py
@@ -33,8 +33,9 @@ guess_col = int(input("Guess Col: "))
 if guess_row == ship_row and guess_col == ship_col:
     print("Congratulations! You sank my battleship!")
 else:
-    print("You missed my battleship!")
-    board[guess_row][guess_col] = "X"
-    print_board(board)
     if (guess_row not in range(5) or guess_col not in range(5)):
-        print("Oops, that's not even in the ocean.")
+    print("Oops, that's not even in the ocean.")
+    else:
+        print("You missed my battleship!")
+        board[guess_row][guess_col] = "X"
+        print_board(board)
