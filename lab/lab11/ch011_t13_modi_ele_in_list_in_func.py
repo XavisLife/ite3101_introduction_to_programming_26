c0931207407da8dc7edfd11bcf2687422fd1250b
XavisGameDev <xavislife2027@outlook.com>
2026-09-10 04:41:36 +0000

    [skip actions] lab/lab11/ch011_t13_modi_ele_in_list_in_func.py

--- lab/lab11/ch011_t13_modi_ele_in_list_in_func.py
+++ lab/lab11/ch011_t13_modi_ele_in_list_in_func.py
@@ -3,9 +3,9 @@ from typing import List
 
 n = [3, 5, 7]
 
-def double_list(x: List[int]) -> List[int]
-for i in range(0, len(n)):
-    n[i] = n[i] * 2
+def double_list(x: List[int]) -> List[int]:
+    for i in range(0, len(x)):
+        x[i] = x[i] * 2
 # Don't forget to return your new list!
 
 # print(double_list(n))
