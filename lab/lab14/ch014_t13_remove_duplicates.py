a100efe361db65ee3f13b168ffb24f8929f7fc0e
XavisGameDev <xavislife2027@outlook.com>
2026-09-10 11:38:45 +0000

    lab/lab14/ch014_t13_remove_duplicates.py

--- lab/lab14/ch014_t13_remove_duplicates.py
+++ lab/lab14/ch014_t13_remove_duplicates.py
@@ -1,4 +1,6 @@
 def remove_duplicates(numbers: list[int]) -> list[int]:
     new_list = []
     for number in numbers:
-        if 
+        if number not in new_list:
+            new_list.append(number)
+    return new_list
