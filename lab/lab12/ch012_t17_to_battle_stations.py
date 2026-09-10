d7271a6e70215e2b6b4036ae7dc729d09f627212
XavisGameDev <xavislife2027@outlook.com>
2026-09-10 10:09:10 +0000

    [skip actions] lab/lab12/ch012_t17_to_battle_stations.py

--- lab/lab12/ch012_t17_to_battle_stations.py
+++ lab/lab12/ch012_t17_to_battle_stations.py
@@ -34,7 +34,7 @@ for turn in range(4):
     guess_col = int(input("Guess Col: "))
 
     if guess_row == ship_row and guess_col == ship_col:
-        print("Congratulations! You sank my battleship!")
+        # print("Congratulations! You sank my battleship!")
         break
     else:
         if guess_row not in range(5) or guess_col not in range(5):
