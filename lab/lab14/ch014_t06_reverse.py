f16c80157fbd13772f98ede98346f2c13b055e05
XavisGameDev <xavislife2027@outlook.com>
2026-09-10 10:41:56 +0000

    lab/lab14/ch014_t06_reverse.py

--- lab/lab14/ch014_t06_reverse.py
+++ lab/lab14/ch014_t06_reverse.py
@@ -1,5 +1,5 @@
 def reverse(text):
     reversed_text = ""
-    for i in range(Len(text) - 1, -1, -1):
+    for i in range(len(text) - 1, -1, -1):
         reversed_text += text[i]
     return reversed_text
