e55ae3959584b697db786a0997ccd7290270bc8a
XavisGameDev <xavislife2027@outlook.com>
2026-09-10 08:59:19 +0000

    lab/lab12/ch012_t06_hide.py

--- lab/lab12/ch012_t06_hide.py
+++ lab/lab12/ch012_t06_hide.py
@@ -12,5 +12,11 @@ def print_board(board_in: List[List[str]]) -> None:
         print(" ".join(row))
 
 # Add your code below!
+
+
 def random_row(board_in: List[List[str]]) -> int:
     return randint(0, len(board_in) - 1)
+
+
+def random_col(board_in: List[List[str]]) -> int:
+    return randint(0, len(board_in[0]) - 1)
