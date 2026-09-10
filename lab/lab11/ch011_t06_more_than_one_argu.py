993cff25e2b34aaf7d359a90ab4650b808ab4833
XavisGameDev <xavislife2027@outlook.com>
2026-09-10 04:23:08 +0000

    lab/lab11/ch011_t06_more_than_one_argu.py

--- lab/lab11/ch011_t06_more_than_one_argu.py
+++ lab/lab11/ch011_t06_more_than_one_argu.py
@@ -1,7 +1,10 @@
 m = 5
 n = 13
 # Add add_function here!
-def add_function(x: int,y: int) -> int:
-    
 
-# print(add_function(m, n))
+
+def add_function(x: int, y: int) -> int:
+    return x + y
+
+
+print(add_function(m, n))
