556db4a5aef8e76ab87a2bce24d7e8e685048bd1
XavisGameDev <xavislife2027@outlook.com>
2026-09-10 10:55:00 +0000

    [skip actions] lab/lab14/ch014_t08_scrabble_score.py

--- lab/lab14/ch014_t08_scrabble_score.py
+++ lab/lab14/ch014_t08_scrabble_score.py
@@ -4,5 +4,6 @@ score = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
          "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4,
          "x": 8, "z": 10}
 
+
 def scrabble_score(word: str) -> int:
-    return score[word].lower()
+    return score[word.lower()]
