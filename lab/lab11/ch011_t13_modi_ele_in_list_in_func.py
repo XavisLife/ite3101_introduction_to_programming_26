e3283d28eda0c86f7ac07f9a0f7bdba01d7705b8
XavisGameDev <xavislife2027@outlook.com>
2026-09-10 05:05:12 +0000

    [skip actions] lab/lab11/ch011_t13_modi_ele_in_list_in_func.py

--- lab/lab11/ch011_t13_modi_ele_in_list_in_func.py
+++ lab/lab11/ch011_t13_modi_ele_in_list_in_func.py
@@ -1,18 +1,10 @@
 n = [3, 5, 7]
 
-"""
+
 def double_list(x):
     for i in range(0, len(x)):
         x[i] = x[i] * 2
     return x
-"""
-
-
-def double_list(x):
-    new_list = []
-    for item in x:
-        new_list.append(item * 4)
-    return new_list
 
 
 print(double_list(n))
