442b58b4035401e635ee044c511d7df66f23d992
XavisGameDev <xavislife2027@outlook.com>
2026-09-10 10:06:58 +0000

    lab/lab12/ch012_t15_game_over.py

--- lab/lab12/ch012_t15_game_over.py
+++ lab/lab12/ch012_t15_game_over.py
@@ -43,6 +43,6 @@ for turn in range(4):
         else:
             print("You missed my battleship!")
             board[guess_row][guess_col] = "X"
-        print_board(board)
         if turn == 3:
             print("Game Over")
+        print_board(board)
