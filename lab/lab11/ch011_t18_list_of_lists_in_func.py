32fbd13b282e06b2e7cf2bd84fe981dee71405f8
XavisGameDev <xavislife2027@outlook.com>
2026-09-10 05:24:44 +0000

    [skip actions] lab/lab11/ch011_t18_list_of_lists_in_func.py

--- lab/lab11/ch011_t18_list_of_lists_in_func.py
+++ lab/lab11/ch011_t18_list_of_lists_in_func.py
@@ -3,5 +3,7 @@ n = [[1, 2, 3], [4, 5, 6, 7, 8, 9]]
 def flatten(lists):
     results = []
     for numbers in lists:
+        for number in numbers:
+            results.append(number)
 
 # print(flatten(n))
