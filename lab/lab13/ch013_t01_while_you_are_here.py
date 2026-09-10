2af32cba6392539a0206a73d3f4ec2df5a35460d
XavisGameDev <xavislife2027@outlook.com>
2026-09-10 02:41:41 +0000

    [skip actions] lab/lab13/ch013_t01_while_you_are_here.py

--- lab/lab13/ch013_t01_while_you_are_here.py
+++ lab/lab13/ch013_t01_while_you_are_here.py
@@ -1,8 +1,8 @@
 count = 0
 
-if count < 5:
+if count < 9:
     print("Hello, I am an if statement and count is", count)
 
-while count < 5:
+while count < 9:
     print("Hello, I am a while and count is", count)
     count += 1
