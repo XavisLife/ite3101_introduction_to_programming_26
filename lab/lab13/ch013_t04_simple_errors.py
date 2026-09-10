93a3a02859ccb7ab3145e99cf6d0c9b59c3e9ac0
XavisGameDev <xavislife2027@outlook.com>
2026-09-10 02:47:51 +0000

    lab/lab13/ch013_t04_simple_errors.py

--- lab/lab13/ch013_t04_simple_errors.py
+++ lab/lab13/ch013_t04_simple_errors.py
@@ -1,4 +1,4 @@
 choice = input('Enjoying the course? (y/n)')
 
-while False:  # Fill in the condition (before the colon)
+while choice != 'y' and choice != 'n':  # Fill in the condition (before the colon)
     choice = input("Sorry, I didn't catch that. Enter again: ")
