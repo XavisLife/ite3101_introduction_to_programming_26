ad5de5909aedd69505b1d4999055634086f94491
XavisGameDev <xavislife2027@outlook.com>
2026-09-10 03:18:59 +0000

    [skip actions] lab/lab13/ch013_t10_for_yr_hobbies.py

--- lab/lab13/ch013_t10_for_yr_hobbies.py
+++ lab/lab13/ch013_t10_for_yr_hobbies.py
@@ -3,9 +3,9 @@ hobbies = []
 # Add your code below!
 for hobby in hobbies:
     hobby = (input("What is your hobby?"))
-    hobbies.append(hobby)
     x += 1
     if (x > 3):
         break
 
+hobbies.append(hobby)
 print(hobbies)
