f27aaaacb534000d3aea170ee1e6365bc77b0e54
XavisGameDev <xavislife2027@outlook.com>
2026-09-10 11:42:39 +0000

    [skip actions] lab/lab14/ch014_t14_median.py

--- lab/lab14/ch014_t14_median.py
+++ lab/lab14/ch014_t14_median.py
@@ -3,3 +3,5 @@ def median(numbers: list[int]) -> float:
     middle = len(sorted_list) // 2
     if len(sorted_list) % 2 == 0:
         return (sorted_list[middle - 1] + sorted_list[middle]) / 2
+    else:
+        return sorted_list[middle]
