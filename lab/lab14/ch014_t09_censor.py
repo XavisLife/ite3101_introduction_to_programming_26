cf42c985872c06b53f57d0945bfb122166ec0277
XavisGameDev <xavislife2027@outlook.com>
2026-09-10 11:04:59 +0000

    [skip actions] lab/lab14/ch014_t09_censor.py

--- lab/lab14/ch014_t09_censor.py
+++ lab/lab14/ch014_t09_censor.py
@@ -1,5 +1,5 @@
 def censor(text: str, word: str) -> str:
-    for i in range(lenword:
+    for i in range(len(word)):
         if character in text:
             text = text.replace(character, "*")
     return text
