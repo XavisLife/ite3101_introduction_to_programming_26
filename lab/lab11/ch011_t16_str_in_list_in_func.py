dbedb43e13b62cde1aff08e989a00841b31dabe6
XavisGameDev <xavislife2027@outlook.com>
2026-09-10 05:20:42 +0000

    lab/lab11/ch011_t16_str_in_list_in_func.py

--- lab/lab11/ch011_t16_str_in_list_in_func.py
+++ lab/lab11/ch011_t16_str_in_list_in_func.py
@@ -1,9 +1,12 @@
 n = ["Michael", "Lieberman"]
 # Add your function here
+
+
 def join_strings(words):
     result = ""
     for i in range(0, len(words)):
         result += words[i]
     return result
 
-# print(join_strings(n))
+
+print(join_strings(n))
