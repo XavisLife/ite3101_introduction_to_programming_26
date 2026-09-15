f23f5180027a16a6c0456f37917cd29f0ecd7adf
XavisGameDev <xavislife2027@outlook.com>
2026-09-15 02:19:00 +0000

    [skip actions] lab/lab15/ch015_t03_sum_scores.py

--- lab/lab15/ch015_t03_sum_scores.py
+++ lab/lab15/ch015_t03_sum_scores.py
@@ -1,8 +1,8 @@
 grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
 
-total = 0
-def grades_sum(scores: list) -> int:
 
+def grades_sum(scores: list) -> int:
+    total = 0
     for score in scores:
         total += score
     return total
