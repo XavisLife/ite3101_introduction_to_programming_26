ece659af5c7bfe7249979c0362bd5851732629b0
XavisGameDev <xavislife2027@outlook.com>
2026-09-15 12:04:42 +0000

    [skip actions] lab/lab16/ch016_t18_lambda_expressions.py

--- lab/lab16/ch016_t18_lambda_expressions.py
+++ lab/lab16/ch016_t18_lambda_expressions.py
@@ -1,1 +1,6 @@
+'''
+Create a new variable called message.
+Set it to the result of calling filter() with the appropriate lambda that will filter out the "X"s. The second argument will be garbled.
+Finally, print your message to the console.
+'''
 garbled = "IXXX aXXmX aXXXnXoXXXXXtXhXeXXXXrX sXXXXeXcXXXrXeXt mXXeXsXXXsXaXXXXXXgXeX!XX"
