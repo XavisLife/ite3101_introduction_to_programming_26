2d4d13ae9ffa2b69d4e6067bccc9f5de9533f68f
XavisGameDev <xavislife2027@outlook.com>
2026-09-15 11:40:29 +0000

    lab/lab16/ch016_t13_lambda_syntax.py

--- lab/lab16/ch016_t13_lambda_syntax.py
+++ lab/lab16/ch016_t13_lambda_syntax.py
@@ -1,4 +1,4 @@
 languages = ["HTML", "JavaScript", "Python", "Ruby"]
 
 # Add arguments to the filter()
-print(list(filter(lambda x: , languages)))
+print(list(filter(lambda x: x == "Python", languages)))
