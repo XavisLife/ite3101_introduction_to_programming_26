9edf99025bb39896b5891d626e3338c51b77b1ad
XavisGameDev <xavislife2027@outlook.com>
2026-09-15 02:32:20 +0000

    [skip actions] lab/lab15/ch015_t05_the_variance.py

--- lab/lab15/ch015_t05_the_variance.py
+++ lab/lab15/ch015_t05_the_variance.py
@@ -15,7 +15,8 @@ def print_grades(grades_input: List[float]):
         print(grade)
 
 
-def grades_variance(scores: list) 
+def grades_variance(scores: list):
+    
 def grades_sum(scores: List[float]) -> float:
     total = 0
     for score in scores:
