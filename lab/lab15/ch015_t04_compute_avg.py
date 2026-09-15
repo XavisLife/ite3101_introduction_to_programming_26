5fabaa697bde979220edae6db2b5ebef51401231
XavisGameDev <xavislife2027@outlook.com>
2026-09-15 02:25:17 +0000

    [skip actions] lab/lab15/ch015_t04_compute_avg.py

--- lab/lab15/ch015_t04_compute_avg.py
+++ lab/lab15/ch015_t04_compute_avg.py
@@ -14,6 +14,7 @@ def grades_sum(scores: List[float]) -> float:
         sum += item
     return sum
 
-def grades_average(grades_input: list) ->
+def grades_average(grades_input: list) -> float:
+    
 
 print(grades_sum(grades))
