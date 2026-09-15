06ebab8046b9449c9bf311acc787bdeb794ee8f4
XavisGameDev <xavislife2027@outlook.com>
2026-09-15 02:08:07 +0000

    [skip actions] lab/lab15/ch015_t02_print_those_grades.py

--- lab/lab15/ch015_t02_print_those_grades.py
+++ lab/lab15/ch015_t02_print_those_grades.py
@@ -1,3 +1,5 @@
 grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
-for grade in grades:
-    print(grades[grade]/n)
+
+def print_grades(grades_input: list):
+    for item in grades_input:
+        
