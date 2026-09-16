2d06ed04fc7f496b23f87bf2460d323d85eabb07
XavisGameDev <xavislife2027@outlook.com>
2026-09-16 02:50:43 +0000

    [skip actions] lab/lab20/ch20_t05_read_between_lines.py

--- lab/lab20/ch20_t04_reading.py
+++ lab/lab20/ch20_t04_reading.py
@@ -3,3 +3,4 @@ file_path_name = os.path.join(os.path.dirname(
     os.path.abspath(__file__)), "inputs/ch20_t04o_output.txt")
 my_file = open(file_path_name, "r")
 print(my_file.read())
+my_file.close()
--- lab/lab20/ch20_t05_read_between_lines.py
+++ lab/lab20/ch20_t05_read_between_lines.py
@@ -1,3 +1,7 @@
 import os
 file_path_name = os.path.join(os.path.dirname(
     os.path.abspath(__file__)), "inputs/ch20_t05o_output.txt")
+
+my_file = open(file_path_name, "r")
+print(my_file.readline())
+m
