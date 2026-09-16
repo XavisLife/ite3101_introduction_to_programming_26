de34a8590d330f2f1c355cfee747648387cd2101
XavisGameDev <xavislife2027@outlook.com>
2026-09-16 01:55:15 +0000

    [skip actions] lab/lab19/ch019_t08_modi_mem_var.py

--- lab/lab19/ch019_t08_modi_mem_var.py
+++ lab/lab19/ch019_t08_modi_mem_var.py
@@ -10,6 +10,7 @@ class Car(object):
         print("This is a %s %s with %s MPG." %
               (self.color, self.model, str(self.mpg)))
 
+    def drive_car()
 
 my_car = Car("DeLorean", "silver", 88)
 my_car.display_car()
