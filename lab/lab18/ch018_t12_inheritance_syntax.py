8721abf0484c511a0d84af4f3ed337d18e4783b0
XavisGameDev <xavislife2027@outlook.com>
2026-09-16 01:05:27 +0000

    lab/lab18/ch018_t12_inheritance_syntax.py

--- lab/lab18/ch018_t12_inheritance_syntax.py
+++ lab/lab18/ch018_t12_inheritance_syntax.py
@@ -5,7 +5,10 @@ class Shape(object):
         self.number_of_sides = number_of_sides
 
 # Add your Triangle class below!
+
+
 class Triangle(Shape):
     def __init__(self, side1, side2, side3):
-        self.side = side1
-        se
+        self.side1 = side1
+        self.side2 = side2
+        self.side3 = side3
