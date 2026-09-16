3b556ca2ca4c2c001c53e0fcfde72f786801c97f
XavisGameDev <xavislife2027@outlook.com>
2026-09-16 00:52:41 +0000

    [skip actions] lab/lab18/ch018_t08_methodical_approach.py

--- lab/lab18/ch018_t08_methodical_approach.py
+++ lab/lab18/ch018_t08_methodical_approach.py
@@ -6,9 +6,9 @@ class Animal(object):
         self.name = name
         self.age = age
     # Add your method here!
-    def description(self, name, age):
+    def description(self):
         print(self.name)
         print(self.age)
 
 hippo = Animal("Hippo", 6)
-hippo.description
+hippo.description()
