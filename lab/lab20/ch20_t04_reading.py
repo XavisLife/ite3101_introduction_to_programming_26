cd90371c795c3d4255df3d72b4b911725c172e4c
XavisGameDev <xavislife2027@outlook.com>
2026-09-16 02:47:50 +0000

    lab/lab20/ch20_t04_reading.py

--- lab/lab20/ch20_t04_reading.py
+++ lab/lab20/ch20_t04_reading.py
@@ -2,4 +2,4 @@ import os
 file_path_name = os.path.join(os.path.dirname(
     os.path.abspath(__file__)), "inputs/ch20_t04o_output.txt")
 my_file = open(file_path_name, "r")
-print
+print(my_file.read())
