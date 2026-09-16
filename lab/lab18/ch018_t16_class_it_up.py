cb3254cb8c8f828f0993cddadee1fafb22eb9665
XavisGameDev <xavislife2027@outlook.com>
2026-09-16 01:22:07 +0000

    [skip actions] lab/lab18/ch018_t16_class_it_up.py

--- lab/lab18/ch018_t16_class_it_up.py
+++ lab/lab18/ch018_t16_class_it_up.py
@@ -1,5 +1,12 @@
-class Triangle(object):
+class Triangle(object)
+    number_of_sides = 3
+    
     def __init__(self, angle1: int, angle2: int, angle3: int):
         self.angle1 = angle1
         self.angle2 = angle2
         self.angle3 = angle3
+
+    def check_angles(self):
+        if (angle1 == 180 or angle2 == 180 or angle3 == 180)
+            return True
+        return False
