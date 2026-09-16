278c8bb61fe677b428e0f2ef5a61034c6ea66890
XavisGameDev <xavislife2027@outlook.com>
2026-09-16 01:54:04 +0000

    [skip actions] lab/lab19/ch019_t07_create_class_method.py

--- lab/lab19/ch019_t07_create_class_method.py
+++ lab/lab19/ch019_t07_create_class_method.py
@@ -7,7 +7,7 @@ class Car(object):
         self.mpg = mpg
 
     def display_car(self):
-        mpg_string = (str)self.mpg
+        mpg_string = str(self.mpg))
         return f"This is a {self.color} {self.model} with {self.mpg} MPG."
 
 my_car = Car("DeLorean", "silver", 88)
