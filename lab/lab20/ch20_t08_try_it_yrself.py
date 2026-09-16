d67beccaeb0342b0694c14fd50260eb5da268cac
XavisGameDev <xavislife2027@outlook.com>
2026-09-16 02:59:56 +0000

    [skip actions] lab/lab20/ch20_t08_try_it_yrself.py

--- lab/lab20/ch20_t08_try_it_yrself.py
+++ lab/lab20/ch20_t08_try_it_yrself.py
@@ -2,5 +2,5 @@ import os
 file_path_name = os.path.join(os.path.dirname(
     os.path.abspath(__file__)), "outputs/ch20_t08o_output.txt")
 
-with open(file_path_name, "w") as variable:
+with open(file_path_name, "w") as my_file:
     my_file.write("Success!")
