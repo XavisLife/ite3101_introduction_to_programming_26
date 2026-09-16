01536d3228313fee68870d89d802edafb32a2bd2
XavisGameDev <xavislife2027@outlook.com>
2026-09-16 01:52:04 +0000

    [skip actions] lab/lab19/ch019_t07_create_class_method.py

--- lab/lab19/ch019_t07_create_class_method.py
+++ lab/lab19/ch019_t07_create_class_method.py
@@ -7,7 +7,7 @@ class Car(object):
         self.mpg = mpg
 
     def display_car(self):
-        return f"This is a {}"
+        return f"This is a {self.color}"
 
 my_car = Car("DeLorean", "silver", 88)
 
